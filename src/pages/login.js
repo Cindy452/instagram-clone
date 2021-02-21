c3d031e110b6db34d7217e1f8cfb04e35247bc01
Your Name <you@example.com>
2021-02-20 18:57:13 -0600

    styled signup page and validation

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import image from '../assets/app-iphone-user.png';
 import logo from '../assets/logo.jpg';
-import { Link } from 'react-router-dom';
+import { Link,  useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
 
 export default function Login() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const [error, setError] = useState('');
+    const isInvalid = password === '' || emailAddress === '';
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try {
+            await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+            history.push(ROUTES.DASHBOARD);
+        } catch (error) {
+            setEmailAddress('');
+            setPassword('');
+            setError(error.message);
+        }
+    }
+
+    useEffect(() => {
+        document.title = 'Login - Instagram';
+    }, [])
+    
+
     return (
         <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
          <div className='flex w-3/5'>
@@ -14,20 +41,26 @@ export default function Login() {
                 <h1 className='flex justify-center w-full'>
                     <img src={logo} alt='logo' className='mt-2 w-6/12 mb-4' />
                 </h1>
-                <form method='POST'>
+                {error && <p className="mb-4 text-xs text-red-500">{error}</p>}
+                <form onSubmit={handleLogin} method='POST'>
                     <input
                         aria-label='Enter your email address'
                         className='text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2'
                         type='text'
                         placeholder='Email address'
+                        value={emailAddress}
+                        onChange={({ target }) => setEmailAddress(target.value)}
                     />
                     <input
                         aria-label='Enter your password'
                         className='text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2'
                         type='password'
                         placeholder='Password'
+                        value={password}
+                        onChange={({ target }) => setPassword(target.value)}
                     />
                     <button
+                        disabled={isInvalid}
                         type='submit'
                         className='bg-blue-500 text-white w-full rounded h-8 font-bold'
                     >Log In
